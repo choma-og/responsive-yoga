04abd36e2137e31a544c00c45bad031e939d0f5f
choma-og <destroksie@icloud.com>
2023-04-10 22:15:25 +0700

    added Breakpoints and ScrollReveal
    Breakpoints and ScrollReveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,6 +72,7 @@ window.addEventListener('scroll', scrollActive)
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'ri-sun-line'
+
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
@@ -96,3 +97,16 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal(`.home__data, .list__container, .join__content, .footer__container`)
+sr.reveal(`.home__img`, {origin: 'bottom'})
+sr.reveal(`.health__image, .routine__images, .follow__img-3`, {origin: 'left'})
+sr.reveal(`.health__data, .routine__data, .follow__img-4`, {origin: 'right'})
+sr.reveal(`.follow__data, .follow__content-1`, {interval: 100})
